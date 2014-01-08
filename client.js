045d55fb0dabd358882b1d22e84204053acc136b
Matt Dobson <mdobson4@gmail.com>
2014-01-08 10:35:24 -0500

    Client catchup.

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 var Fog = require('thefog'),
     Packet = Fog.Packet;
 
-var client = new Fog.Client({'endpoint':'ws://0.0.0.0:3000'});
+var client = new Fog.Client({'endpoint':'ws://thefog.herokuapp.com'});
 
 client.open(function() {
   console.log('Client opened!');
